0e6f4b2171e4d631723ce7889d909aa3ae209b42
Jason Oesin <91514784+jasonoesin@users.noreply.github.com>
2023-07-28 14:26:26 +0700

    get loan

--- backend/loan/views.py
+++ backend/loan/views.py
@@ -45,7 +45,7 @@ class _CreateLoanView(APIView):
         loan_approval_id = request.data.get('approval')
         loan_approval = get_object_or_404(LoanApproval, pk=loan_approval_id)
 
-        serializer.save()
+        
 
         # Get Bearer Token
         bearer_token = request.META.get('HTTP_AUTHORIZATION', '').split('Bearer ')[1]
@@ -61,11 +61,28 @@ class _CreateLoanView(APIView):
 
         if response.status_code // 100 == 2:
             response_data = response.json()
+            serializer.save()
             return success_response(data=response_data)
         else:
             return error_response(error_message=response.text)
         
+class _GetLoan(APIView):
+    def get(self, request):
+        user_id = self.request.GET.get("user_id")
+
+        try:
+            loan_approval = LoanApproval.objects.get(user_id=user_id, is_approved=True)
+            loan = Loan.objects.get(approval=loan_approval, is_payed=False)
+            serializer = LoanSerializer(loan)
+            return success_response(serializer.data)
+        except LoanApproval.DoesNotExist:
+            return error_response("Loan approval not found for the user.")
+        except Loan.DoesNotExist:
+            return error_response("No unpaid loan found for the user.")
+        
 create_loan_approval_view = _CreateLoanApproval.as_view()
 approve_loan_approval_view = _ApproveLoanApproval.as_view()
 create_loan_view = _CreateLoanView.as_view()
+get_loan_view = _GetLoan.as_view()
+
 
